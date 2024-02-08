718f4cb651568a7684cfdbc930c26603041341ca
Noor Fatima <50588372+NNoorFatima@users.noreply.github.com>
2024-02-09 03:25:03 +0500

    Create diamond.cpp

---
+++ pattern_printing/diamond.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+  for (int row = 1; row <= 5; row++)
+	{
+	  for (int col = 5; col > 0; col--)
+		{
+		  if (col < row)
+			cout << "* ";
+		  else
+			cout << " ";
+		}
+	  cout << endl;
+	}
+
+  for (int row = 1; row <= 5; row++)
+	{
+	  for (int col = 1; col <= 5; col++)
+		{
+		  if (col < row)
+			{
+			  cout << " ";
+			}
+		  else
+			{
+			  cout << "* ";
+			}
+		}
+	  cout << endl;
+	}
+}
+
