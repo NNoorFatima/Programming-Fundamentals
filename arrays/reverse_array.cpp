fec3fe45f49c66a619465ebc2af1e6b2af7e6caf
Noor Fatima <50588372+NNoorFatima@users.noreply.github.com>
2024-02-10 23:43:41 +0500

    Update reverse_array.cpp

--- arrays/reverse_array.cpp
+++ arrays/reverse_array.cpp
@@ -1,16 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
 int main()
 {
-    int num[]={1,2,3,4,5,6,7,8,9,10};
+    int num[]={1,2,3,4,5,6};
+    int k;
     const int size= sizeof(num)/sizeof(num[0]);
-    int j=size-1;
-    int i=0;
-    int temp=0;
-    //reversing array---------------------------
-   while (i<size/2 && j>=4)
+    k=size-1;
+    //reversing array
+    for(int i=0 ;i<size/2 ;i++,k--)
     {
-        temp=num[i];
-        num[i]=num[j];
-        num[j]=temp;
-        i++;
-        j--;
+        int temp= num[i];
+        num[i]=num[k];
+        num[k]=temp;
+        
     }
+    
+    
+    for(int i=0 ;i<size ;i++)
+        std::cout <<num[i] << std::endl;
+}
